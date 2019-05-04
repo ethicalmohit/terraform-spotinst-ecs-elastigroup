c97e7c002b36c44f988eff2870f9231fd7d5a53d
ethicalmohit <mail@imohit.me>
2019-05-04 11:14:28 +0530

    Added spotinst credentials variables.

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "spotinst_token" {
+	description = "describe your variable"
+	default     = "default value"
+}
+
+variable "spotinst_account" {
+	description = "describe your variable"
+	default     = "default value"
+}
