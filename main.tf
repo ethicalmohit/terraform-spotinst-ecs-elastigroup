33a8e9ecdde647d9962af432fd25192581d02912
ethicalmohit <mail@imohit.me>
2019-05-04 17:20:36 +0530

    Added spotinst credentials in root module.

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-# module "spotinst" {
-#   source           = "./modules/elastigroup"
-#   spotinst_account = "act-9e1cbeb5"
-#   spotinst_token   = "ba741b8509d525bc09c5e6ca4ea7610686c081a8bbbe3ad6625d1ba748ec7a15"
-# }
+module "spotinst" {
+  source           = "./modules/elastigroup"
+  spotinst_account = "act-9e1cbeb5"
+  spotinst_token   = "ba741b8509d525bc09c5e6ca4ea7610686c081a8bbbe3ad6625d1ba748ec7a15"
+}
 
 module "iam-role" {
   source = "./modules/iam"
