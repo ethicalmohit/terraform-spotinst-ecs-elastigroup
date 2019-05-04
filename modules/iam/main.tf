f8e4d2bd637fce2cec7c86f744aca04aed9f043c
ethicalmohit <mail@imohit.me>
2019-05-04 13:17:23 +0530

    Removing resource policy from the assume role policy.

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -4,13 +4,21 @@ resource "aws_iam_role" "external" {
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
-  "Statement": {
-    "Effect": "Allow",
-    "Principal": {
-      "AWS": "arn:aws:iam::922761411349:*"
-    },
-    "Action": "sts:AssumeRole"
-  }
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::922761411349:root"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "StringEquals": {
+          "sts:ExternalId": "spotinst:aws:extid:3w72ms42i8000000"
+        }
+      }
+    }
+  ]
 }
 EOF
 }
+
