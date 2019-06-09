907e30f1533a3cad51d0c0cfbfaab0fc0200ffc0
ethicalmohit <mail@imohit.me>
2019-06-09 22:22:57 +0530

    Adding clustername in root module.

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "spotinst" {
   source           = "./modules/elastigroup"
   spotinst_account = "act-9e1cbeb5"
   spotinst_token   = "ba741b8509d525bc09c5e6ca4ea7610686c081a8bbbe3ad6625d1ba748ec7a15"
+  ecs_cluster_name = "testing"
 }
 
 module "iam-role" {
